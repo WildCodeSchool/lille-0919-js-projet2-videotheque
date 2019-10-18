aaeecc10a535dd1d3d4a008f5ba398ab62126404
Nicolas Merlier <nicolasmerlier@gmail.com>
2019-10-18 16:52:38 +0200

    j'ai créé la div du footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return <Footer />;
 }
 
 export default App;
