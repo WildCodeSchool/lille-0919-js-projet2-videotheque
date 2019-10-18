3fefe6449494d44dbfa6340622f50e6f09d56f49
jervalles <jer.vallles@outlook.com>
2019-10-18 23:50:10 +0200

    Movie description added on the MoviePage component

--- frontend/src/components/MoviePage.jsx
+++ frontend/src/components/MoviePage.jsx
@@ -6,12 +6,27 @@ const MoviePage = () => (
     <div className="movieTitleContainer">
       <div className="movieMainJacket">
         <img
-          className="movieMainJAcketImg"
+          className="movieMainJacketImg"
           src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
         ></img>
       </div>
-      <div></div>
+      <div className="movieDescription">
+        <h2>Film title</h2>
+        <p>
+          <span className="oneRedWord">By</span> John Café
+        </p>
+        <p>
+          <span className="oneRedWord">With</span> Anna Blanders, Jim Carrey...
+        </p>
+        <p>
+          <span className="oneRedWord">Released at</span> 1 January, 2019
+        </p>
+        <p>
+          <span className="oneRedWord">Runtime</span> 1h30
+        </p>
+      </div>
     </div>
+    <hr></hr>
   </div>
 );
 
