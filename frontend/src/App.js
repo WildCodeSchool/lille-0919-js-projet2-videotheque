34e0c9443ad3cb31ddf3ede5430f18c59569939c
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-20 23:30:53 +0200

    import ListMovies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ListMovies from "./components/ListMovies";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ListMovies />
+    </div>
+  );
 }
 
 export default App;
