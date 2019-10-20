4cc978b041027fa26179738fdaf40a02f690567d
Nicolas Merlier <nicolasmerlier@gmail.com>
2019-10-20 20:38:54 +0200

    création des liens copyright, about us, twitter et contact us dans le footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Footer.css";
 
-function footer() {
-  return <div className="footer"></div>;
+function Footer() {
+  return (
+    <div className="Footer">
+      <ul>
+        <li a href="contact_us">
+          Contact us
+        </li>
+        <li a href="twitter">
+          Twitter
+        </li>
+        <li a href="about_us">
+          About us
+        </li>
+        <li a href="copyright">
+          Copyright
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default footer;
+export default Footer;
