665082cd43b35ecf8f829114d7b2929015e27419
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-20 16:11:17 +0200

    add picture in relation with the genre

--- frontend/src/components/GenreList.jsx
+++ frontend/src/components/GenreList.jsx
@@ -5,7 +5,7 @@ const GenresList = [
   {
     name: "Action",
     background:
-      "http://fr.web.img6.acsta.net/r_640_360/newsv7/17/09/20/10/53/5922358.jpg"
+      "https://www.lemagducine.fr/wp-content/uploads/2019/10/will-smith-ang-lee-gemini-man-1155x720.jpg"
   },
   {
     name: "Animation",
@@ -13,24 +13,13 @@ const GenresList = [
       "http://fr.web.img6.acsta.net/r_640_360/newsv7/17/09/20/10/53/5922358.jpg"
   },
   {
-    name: "classics",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
-  },
-  {
     name: "Comedy",
     background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
-  },
-  {
-    name: "Documentary",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
+      "http://www.premiere.fr/sites/default/files/styles/scale_crop_1280x720/public/2018-04/Very-Bad-Trip-3-les-trois-acteurs-rempilent-pour-45-millions-de-dollars.jpg"
   },
   {
     name: "Drama",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
+    background: "https://thumb.canalplus.pro/bddpe/unsafe/1280x720/52841507"
   },
   {
     name: "Horror",
@@ -40,27 +29,16 @@ const GenresList = [
   {
     name: "Kids & Family",
     background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
-  },
-  {
-    name: "Mystery",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
+      "http://www.pieuvre.ca/wp-content/uploads/2019/08/DetecivePikachu_poster.jpg"
   },
   {
     name: "Romance",
     background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
+      "https://image.jimcdn.com/app/cms/image/transf/dimension=555x10000:format=jpg/path/s63ccf77ce0e97436/image/i8528a6160814ca02/version/1550906294/image.jpg"
   },
   {
     name: "Sci-fi & Fantasy",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
-  },
-  {
-    name: "Horror",
-    background:
-      "http://fr.web.img5.acsta.net/r_640_360/newsv7/18/01/27/13/27/3768109.jpg"
+    background: "https://www.syfy.com/sites/syfy/files/wire/legacy/x-wing.0.jpg"
   }
 ];
 
