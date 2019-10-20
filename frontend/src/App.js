5014ba778a69d9292c8ada7eea64b2370814385b
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-10-20 13:31:30 +0200

    topBar mobile version  first commit but problems with import and export in app.js i don't understand the problem we should see that soon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import topBar from "./components/topBar";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <topBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/topBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./topBar.css";
+
+const topBar = () => (
+  <header className="topBar">
+    <nav className="topBar_navigation">
+      <div className="topBar_logo">
+        <a href="homeWebsite">MyMovies</a>
+      </div>
+
+      <div className="spacer" />
+
+      <div className="topBar_navigation_items">
+        <ul>
+          <li>
+            <a href="userConnexion">
+              <img
+                id="logoUser"
+                alt="logoUser"
+                src={require("./components/logoUser.jpg")}
+                onclick="location.href='http://www.allocine.fr/film/fichefilm_gen_cfilm=61282.html'"
+              />
+            </a>
+          </li>
+          <li>
+            <button className="searchBarButton">
+              <img
+                id="pictoLoupe"
+                alt="pictoLoupe"
+                src={require("./components/pictoLoupe.png")}
+              />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default topBar;
+
+/*<img
+  id="logoWebsite"
+  alt="logoWebsite"
+  src=""
+  onclick="location.href='http://www.allocine.fr/film/fichefilm_gen_cfilm=61282.html'"
+  />*/
