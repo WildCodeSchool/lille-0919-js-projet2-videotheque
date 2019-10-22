259a9585754c5e4a885d1b1b5126add5b8228075
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-22 09:15:11 +0200

    forget a return in the function

--- frontend/src/components/ListMovies.jsx
+++ frontend/src/components/ListMovies.jsx
@@ -4,24 +4,26 @@ import movieList from "../DataBaseMovie";
 import "./ListMovies.css";
 
 function ListMovies() {
-  <div className="ListMovies">
-    <h1>Genre</h1>
-    {movieList
-      .filter(toto => {
-        return toto.genre === "Horror";
-      })
-      .map(movie => (
-        <a href=" ">
-          <Movie
-            key={movie.title}
-            title={movie.title}
-            genre={movie.genre}
-            duration={movie.duration}
-            picture={movie.picture}
-            synopsis={movie.synopsis}
-          />
-        </a>
-      ))}
-  </div>;
+  return (
+    <div className="ListMovies">
+      <h1>Genre</h1>
+      {movieList
+        .filter(toto => {
+          return toto.genre === "Horror";
+        })
+        .map(movie => (
+          <a href=" ">
+            <Movie
+              key={movie.title}
+              title={movie.title}
+              genre={movie.genre}
+              duration={movie.duration}
+              picture={movie.picture}
+              synopsis={movie.synopsis}
+            />
+          </a>
+        ))}
+    </div>
+  );
 }
 export default ListMovies;
