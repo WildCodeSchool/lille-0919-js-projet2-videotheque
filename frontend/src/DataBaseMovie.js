3c4f9126bbc928427639c9b87499871ee3d6f701
jervalles <jer.vallles@outlook.com>
2019-10-23 17:54:03 +0200

    modal-react installed and working by clicking on the trailer image. New CSS created for this Modal. It is responsive with triple format: Phone, Tablet and Desktop

--- frontend/src/DataBaseMovie.js
+++ frontend/src/DataBaseMovie.js
@@ -112,7 +112,7 @@ const movieList = [
     picture:
       "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lbGzEyESjANpOeD48aROlc3X7aa.jpg",
     duration: "2h32",
-    genre: "Horror",
+    genre: "Action",
     synopsis:
       "Encore profondément marqué par le traumatisme qu'il a vécu, enfant, à l'Overlook Hotel, Dan Torrance a dû se battre pour tenter de trouver un semblant de sérénité. Mais quand il rencontre Abra, courageuse adolescente aux dons extrasensoriels, ses vieux démons resurgissent. Car la jeune fille, consciente que Dan a les mêmes pouvoirs qu'elle, a besoin de son aide : elle cherche à lutter contre la redoutable Rose Claque et sa tribu du Nœud Vrai qui se nourrissent des dons d'innocents comme elle pour conquérir l'immortalité. Formant une alliance inattendue, Dan et Abra s'engagent dans un combat sans merci contre Rose. Face à l'innocence de la jeune fille et à sa manière d'accepter son don, Dan n'a d'autre choix que de mobiliser ses propres pouvoirs, même s'il doit affronter ses peurs et réveiller les fantômes du passé… ",
     releasedDate: "1 january, 2019",
--- frontend/src/components/MoviePage.jsx
+++ frontend/src/components/MoviePage.jsx
@@ -1,54 +1,91 @@
 import React from "react";
 import "./MoviePage.css";
+import "./MoviePageTrailer.css";
+import Modal from "react-modal";
 
-function MoviePage(props) {
-  return (
-    <div id="around">
-      <div id="movieTrailerContainer">
-        <img src={props.trailerThumb} />
-      </div>
-      <div id="movieTitleContainer">
-        <div id="movieMainJacket">
-          <img
-            id="movieMainJacketImg"
-            src={props.picture}
-            alt={props.title}
-          ></img>
+class MoviePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <div id="around">
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          className="customStyles"
+          contentLabel="Example Modal"
+          onRequestClose={this.closeModal}
+        >
+          <iframe
+            src="https://www.youtube.com/embed/t433PEQGErc"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <button className="closeTrailer" onClick={this.closeModal}>
+            close
+          </button>
+        </Modal>
+        <div id="movieTrailerContainer">
+          <img src={this.props.trailerThumb} onClick={this.openModal} />
         </div>
-        <div id="movieDescAndIcons">
-          <div id="movieDescription">
-            <h2>{props.title}</h2>
-            <p>
-              <span className="oneRedWord">By</span> {props.by}
-            </p>
-            <p>
-              <span className="oneRedWord">With</span> {props.with}...
-            </p>
-            <p>
-              <span className="oneRedWord">Genre</span> {props.genre}
-            </p>
-            <p>
-              <span className="oneRedWord">Released at</span>{" "}
-              {props.releasedDate}
-            </p>
-            <p>
-              <span className="oneRedWord">Runtime</span> {props.duration}
-            </p>
+        <div id="movieTitleContainer">
+          <div id="movieMainJacket">
+            <img
+              id="movieMainJacketImg"
+              src={this.props.picture}
+              alt={this.props.title}
+            ></img>
           </div>
-          <div id="movieIconsContainer">
-            <img id="moviePlusIcons" src="./plusIcon.png" />
-            <img id="movieLikeIcons" src="./likeIcon.png" />
-            <img id="movieNavetIcons" src="./navetIcon.png" />
+          <div id="movieDescAndIcons">
+            <div id="movieDescription">
+              <h2>{this.props.title}</h2>
+              <p>
+                <span className="oneRedWord">By</span> {this.props.by}
+              </p>
+              <p>
+                <span className="oneRedWord">With</span> {this.props.with}...
+              </p>
+              <p>
+                <span className="oneRedWord">Genre</span> {this.props.genre}
+              </p>
+              <p>
+                <span className="oneRedWord">Released at</span>{" "}
+                {this.props.releasedDate}
+              </p>
+              <p>
+                <span className="oneRedWord">Runtime</span>{" "}
+                {this.props.duration}
+              </p>
+            </div>
+            <div id="movieIconsContainer">
+              <img id="moviePlusIcons" src="./plusIcon.png" />
+              <img id="movieLikeIcons" src="./likeIcon.png" />
+              <img id="movieNavetIcons" src="./navetIcon.png" />
+            </div>
           </div>
         </div>
+        <div id="synopsisContainer">
+          <hr />
+          <h3>Synopsis</h3>
+          <p>{this.props.synopsis}</p>
+        </div>
       </div>
-      <div id="synopsisContainer">
-        <hr />
-        <h3>Synopsis</h3>
-        <p>{props.synopsis}</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MoviePage;
