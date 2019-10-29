73825821f0290788598c8ad4a7c76c011e118571
LaurentMahieu <mahieu.l@hotmail.fr>
2019-10-29 10:59:51 +0100

    transform a href to Link to

--- frontend/src/components/Genre.jsx
+++ frontend/src/components/Genre.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Genre.css";
+import { Link } from "react-router-dom";
 
 function Genre(props) {
   return (
     <div className="genre">
-      <a href=" ">
+      <Link to={`/listMovies/${props.genreName}`}>
+        {" "}
         <img
           key={props.genreName}
           alt={props.genreName}
           src={props.genreBackground}
         />
         <h1>{props.genreName}</h1>
-      </a>
+      </Link>
     </div>
   );
 }
