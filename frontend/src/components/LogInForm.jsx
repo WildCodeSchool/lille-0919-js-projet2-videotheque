f82f42f02403c937828f54ef094d2025be4d03e1
Alice Thammavong <thammavongalice@gmail.com>
2019-11-04 22:10:16 +0100

    Customizing to logIn

--- frontend/src/components/LogInForm.jsx
+++ frontend/src/components/LogInForm.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import "./SignInForm.css";
+import {
+    BrowserRouter as Router,
+    Link,
+    NavLink
+  } from "react-router-dom";
+  import "./LogInForm.css";
 
-class SignInForm extends Component {
+class LogInForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mail: "E-mail",
+
       username: "Username",
-      password: "Password",
-      confPassword: "confPassword"
+      password: "Password"
     };
   }
 
@@ -17,47 +21,33 @@ class SignInForm extends Component {
       <container className="container">
                 
         <div className="logIn">
-                    <h1 className="form">Log In</h1>
-                    <label className="label">E-mail</label>
+            <h1 className="form">Log In</h1>
+               <label className="label">Username</label>
                     
           <input
-            className="signInInput"
-            type="text"
-            value={this.state.mail}
-            onChange={event => {
-              this.setState({ mail: event.target.value });
-            }}
-          />
-                    <label className="label">Username</label>
-                    
-          <input
-            className="signInInput"
+            className="logInInput"
             type="text"
             value={this.state.username}
             onChange={event => {
               this.setState({ username: event.target.value });
             }}
           />
-                    <label className="label">Password</label>
+                   <label className="label">Password</label>
                     
           <input
-            className="signInInput"
+            className="logInInput"
             type="text"
             value={this.state.password}
             onChange={event => {
               this.setState({ password: event.target.value });
             }}
           />
-                    <label className="label">Confirm password</label>
-                    
-          <input
-            className="signInInput"
-            type="text"
-            value={this.state.confPassword}
-            onChange={event => {
-              this.setState({ confPassword: event.target.value });
-            }}
-          />
+            <p className="signUp">
+            <NavLink to="/signInForm">
+              Don't have an account yet? Sign Up!
+            </NavLink>
+            >
+          </p>
                     
           <button type="submit" className="submitButton">
                         Submit !           
@@ -70,4 +60,55 @@ class SignInForm extends Component {
   }
 }
 
-export default SignInForm;
+export default LogInForm;
+
+
+
+
+render() {
+    return (
+      <Router className="container">
+        <div className="logIn">
+          <h1 className="form">Log In</h1>
+
+          <label className="label">Username</label>
+          <input
+            className="logInInput"
+            type="text"
+            value={this.state.title}
+            onChange={event => {
+              this.setState({ username: event.target.value });
+            }}
+          />
+          <label className="label">Password</label>
+          <input
+            className="logInInput"
+            type="text"
+            value={this.state.title}
+            onChange={event => {
+              this.setState({ password: event.target.value });
+            }}
+          />
+
+          <p className="signUp">
+            <NavLink to="/signInForm">
+              Don't have an account yet? Sign Up!
+            </NavLink>
+            >
+          </p>
+
+          <button type="submit" className="submitButton">
+            Submit !
+          </button>
+        </div>
+        <Switch>
+          <Route path="/signInForm">
+            <SignInForm />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default LogInForm;
