946e32c6e46d26377f4e988b9598c7f17584ff07
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-05 11:05:03 +0100

    add Link to the good movieSheet

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie(props) {
   return (
     <div className="Movie">
-      <img className="avatar" src={props.picture} alt={props.title} />
-      <div className="infos">
-        <h2>{props.title}</h2>
-        <p>
-          duration: {props.duration}
-          <br />
-          genre: {props.genre}
-        </p>
-        <div className="synopsis">
+      <Link to={`/movieSheet/${props.id}`}>
+        <img className="avatar" src={props.picture} alt={props.title} />
+        <div className="infos">
+          <h2>{props.title}</h2>
           <p>
-            synopsis:
+            duration: {props.duration}
             <br />
-            {props.synopsis}
+            genre: {props.genre}
           </p>
+          <div className="synopsis">
+            <p>
+              synopsis:
+              <br />
+              {props.synopsis}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
