dd3cd188a2dbf27b52707a34c598022cc92a7222
Alice Thammavong <thammavongalice@gmail.com>
2019-11-05 12:01:44 +0100

    Change submit button

--- frontend/src/components/LogInForm.jsx
+++ frontend/src/components/LogInForm.jsx
@@ -43,7 +43,7 @@ class LogInForm extends React.Component {
         </p>
                   
         <button type="submit" className="submitButton">
-                      Submit !           
+          Submit!
         </button>
                 
       </div>
