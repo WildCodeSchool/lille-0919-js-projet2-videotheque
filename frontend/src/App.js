58bccb185eb52adbaa4c2263997d266049652303
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-11-05 11:50:16 +0100

    changement background police titre style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import PlaylistUser from "./components/PlaylistUser";
 import TopBar from "./components/TopBar";
+
 function App() {
   return (
     <div className="App">
--- frontend/src/components/PlaylistUser.jsx
+++ frontend/src/components/PlaylistUser.jsx
@@ -3,13 +3,14 @@ import "./PlaylistUser.css";
 import { Slide } from "react-slideshow-image";
 
 const slideImages = [
-  "http://fr.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/35/14/60/18363837.jpg",
-  "http://fr.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/35/14/60/18363837.jpg",
-  "http://fr.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/35/14/60/18363837.jpg",
+  "http://fr.web.img2.acsta.net/c_215_290/medias/nmedia/18/64/25/05/19199449.jpg",
+  "http://fr.web.img6.acsta.net/c_215_290/medias/nmedia/18/65/19/82/18835952.jpg",
+  "http://fr.web.img6.acsta.net/c_215_290/o_club300-overlay-ok.png_0_se/medias/nmedia/18/70/03/08/19109117.jpg",
   "http://fr.web.img6.acsta.net/r_1280_720/pictures/19/10/11/11/36/5168199.jpg",
-  "http://fr.web.img3.acsta.net/r_1920_1080/medias/nmedia/18/35/14/60/18363837.jpg",
-  "http://fr.web.img6.acsta.net/r_1280_720/pictures/19/10/11/11/36/5168199.jpg",
-  "http://fr.web.img6.acsta.net/r_1280_720/pictures/19/10/11/11/36/5168199.jpg"
+  "http://fr.web.img6.acsta.net/c_215_290/pictures/19/04/03/14/15/2748213.jpg",
+  "http://fr.web.img6.acsta.net/c_215_290/o_club300-overlay-ok.png_0_se/medias/nmedia/18/70/03/08/19109117.jpg",
+  "https://image.tmdb.org/t/p/w600_and_h900_bestv2/lbGzEyESjANpOeD48aROlc3X7aa.jpg",
+  "http://fr.web.img2.acsta.net/c_215_290/medias/nmedia/18/35/41/59/18422600.jpg"
 ];
 
 const properties = {
@@ -24,6 +25,11 @@ const properties = {
 const Slideshow = () => {
   return (
     <div className="slide-container">
+      <div id="myMoviesLibrary">
+        <h1>My Movies Library </h1>
+        <hr></hr>
+      </div>
+
       <div id="movieIconsFavTitre">
         <img id="movieLikeIcons" src="./likeIcon.png" alt="like" />
         <div>
@@ -38,10 +44,10 @@ const Slideshow = () => {
           <div style={{ backgroundImage: `url(${slideImages[3]})` }}></div>
         </div>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[0]})` }}></div>
-          <div style={{ backgroundImage: `url(${slideImages[1]})` }}></div>
-          <div style={{ backgroundImage: `url(${slideImages[2]})` }}></div>
-          <div style={{ backgroundImage: `url(${slideImages[3]})` }}></div>
+          <div style={{ backgroundImage: `url(${slideImages[4]})` }}></div>
+          <div style={{ backgroundImage: `url(${slideImages[5]})` }}></div>
+          <div style={{ backgroundImage: `url(${slideImages[6]})` }}></div>
+          <div style={{ backgroundImage: `url(${slideImages[7]})` }}></div>
         </div>
       </Slide>
       <div id="movieIconsPlusTitre">
@@ -65,7 +71,13 @@ const Slideshow = () => {
           <div style={{ backgroundImage: `url(${slideImages[3]})` }}></div>
         </div>
       </Slide>
-      <h1>Navet</h1>
+      <div id="movieIconsNavetTitre">
+        <img id="movieNavetIcons" src="./navetIcon.png" alt="navet" />
+        <div>
+          <h1>no no no</h1>
+        </div>
+      </div>
+
       <Slide {...properties}>
         <div className="each-slide">
           <div style={{ backgroundImage: `url(${slideImages[0]})` }}></div>
