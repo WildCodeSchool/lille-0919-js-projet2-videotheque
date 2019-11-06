d278be2eeb1e0d56dfdc5558a11a848c23ebb9eb
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-06 12:32:15 +0100

    add Link to movieSheet

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import "./style/Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie(props) {
   return (
-    <div className="Movie">
-      <img
-        className="avatar"
-        src={`https://image.tmdb.org/t/p/w500/${props.picture}`}
-        alt={props.title}
-      />
-      <div className="infos">
-        <h2>{props.title}</h2>
-        <p>
-          release date: {props.release}
-          <br />
-          genre: {props.genre}
-        </p>
-        <div className="synopsis">
+    <Link to={`/movieSheet/${props.id}`}>
+      <div className="Movie">
+        <img
+          className="avatar"
+          src={`https://image.tmdb.org/t/p/w500/${props.picture}`}
+          alt={props.title}
+        />
+        <div className="infos">
+          <h2>{props.title}</h2>
           <p>
-            synopsis:
+            release date: {props.release}
             <br />
-            {props.synopsis}
+            genre: {props.genre}
           </p>
+          <div className="synopsis">
+            <p>
+              synopsis:
+              <br />
+              {props.synopsis}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
