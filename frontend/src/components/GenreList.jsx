506fe1c60e889e7b410205e527ed615df05847d5
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-06 22:36:44 +0100

    call api, and define the good way to show name of genre

--- frontend/src/components/GenreList.jsx
+++ frontend/src/components/GenreList.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import Genre from "./Genre";
-import DataGenresList from "../DataGenre";
+import axios from "axios";
 
-function GenreList() {
-  return (
-    <div className="genreList">
-      {DataGenresList.map((genre, i) => (
-        <Genre
-          key={i}
-          genreName={genre.name}
-          genreBackground={genre.background}
-        />
-      ))}
-    </div>
-  );
+class GenreList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genres: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=495d98b77df65d47fbf7eba028518ed7`
+      )
+      .then(({ data }) => {
+        const genres = data.genres;
+        this.setState({ genres: genres });
+      });
+  }
+
+  render() {
+    return (
+      <div className="genreList">
+        {this.state.genres.map(genre => (
+          <Genre
+            key={genre.id}
+            idGenre={genre.id}
+            genreName={genre.name}
+            genreBackground={genre.background}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default GenreList;
