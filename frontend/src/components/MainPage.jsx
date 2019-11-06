7c54ad1b7f9be6a176d14be10ab95980e68e3335
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-06 15:47:15 +0100

    ajout key to child

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -46,7 +46,7 @@ class MainPage extends React.Component {
         </div>
         <div className="main-poster">
           {this.state.movies.map(movie => {
-            return <MainPageCard movieData={movie} key={movie.poster_path} />;
+            return <MainPageCard movieData={movie} key={movie.id} />;
           })}
         </div>
       </div>
