4701b6e16ebae2db97a2bb063892bea57e410cab
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-11-06 09:25:11 +0100

    integrating TrailerHomePage page

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import "./style/MainPage.css";
 import MainPageCard from "./MainPageCard";
 import movieList from "../DataBaseMovie";
+import TrailerHomePage from "./TrailerHomePage";
 
 function MainPage() {
   return (
     <div className="main-homepage">
       <div className="trailer-homepage">
-        <div className="trailer">
-          <img
-            className="icon-play"
-            src="pictures/icon-play.svg"
-            alt="Video Player"
-          />
-        </div>
+        <TrailerHomePage />
       </div>
       <div className="main-poster">
         {movieList.map((item, i) => (
