2845b28f5f09c1e8024d5f1a554a38a157b509f6
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-06 09:56:07 +0100

    call axios, function transform to class, call api

--- frontend/src/components/ListMovies.jsx
+++ frontend/src/components/ListMovies.jsx
@@ -1,31 +1,60 @@
 import React from "react";
 import Movie from "./Movie";
-import movieList from "../DataBaseMovie";
+import axios from "axios";
 import "./style/ListMovies.css";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 
-function ListMovies() {
-  const { genreName } = useParams();
-  return (
-    <div className="ListMovies">
-      <h1>{genreName}</h1>
-      {movieList
-        .filter(movie => {
-          return movie.genre === `${genreName}`;
-        })
-        .map(movie => (
-          <a href=" ">
-            <Movie
-              key={movie.title}
-              title={movie.title}
-              genre={movie.genre}
-              duration={movie.duration}
-              picture={movie.picture}
-              synopsis={movie.synopsis}
-            />
-          </a>
-        ))}
-    </div>
-  );
+class ListMovies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  getMovie() {
+    for (let i = 1; i <= 20; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=495d98b77df65d47fbf7eba028518ed7&language=en-US&page=${i}`
+        )
+        .then(({ data }) => {
+          const { results } = data;
+          let tmpMovies = this.state.movies;
+          tmpMovies.push(...results);
+          this.setState({
+            movies: tmpMovies
+          });
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div className="ListMovies">
+        <h1>All Movies</h1>
+        {this.state.movies /*
+          .filter(movie => {
+            return movie.genre === `${this.state.genreName}`;
+          })*/
+          .map(movie => (
+            <a href=" ">
+              <Movie
+                key={movie.title}
+                title={movie.title}
+                genre={movie.genre}
+                duration={movie.duration}
+                picture={movie.picture}
+                synopsis={movie.synopsis}
+              />
+            </a>
+          ))}
+      </div>
+    );
+  }
 }
 export default ListMovies;
