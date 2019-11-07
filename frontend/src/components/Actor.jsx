e70c959da5745b0cf775f141e7f72487c13c16c4
Alice Thammavong <thammavongalice@gmail.com>
2019-11-07 13:55:21 +0100

    Add API in src

--- frontend/src/components/Actor.jsx
+++ frontend/src/components/Actor.jsx
@@ -4,7 +4,10 @@ import "./style/ActorsList.css";
 function Actor(props) {
   return (
     <li>
-      <img src={props.image} alt={props.image} />
+      <img
+        src={`https://image.tmdb.org/t/p/w500${props.image}`}
+        alt={props.image}
+      />
       <p className="actorName">{props.name}</p>
       <p>{props.role}</p>
     </li>
