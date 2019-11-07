8c479754886f85070cc5d3dc7543c665b047eb77
LaurentMahieu <mahieu.l@hotmail.fr>
2019-11-07 13:49:25 +0100

    ajust CSS

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -5,17 +5,17 @@ import { Link } from "react-router-dom";
 function Movie(props) {
   return (
     <Link to={`/movieSheet/${props.id}`}>
-      <div className="Movie">
-        <img
-          className="avatar"
-          src={`https://image.tmdb.org/t/p/w500/${props.picture}`}
-          alt={props.title}
-        />
+      <div className="header">
+        <div className="avatar">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${props.picture}`}
+            alt={props.title}
+          />
+        </div>
         <div className="infos">
           <h2>{props.title}</h2>
+          <p>release date: {props.release}</p>
           <p>
-            release date: {props.release}
-            <br />
             genre:{" "}
             {props.genre.map(genre_id => {
               return `${genre_id} `;
