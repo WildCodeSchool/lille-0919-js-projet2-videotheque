095230ca86a4d3510a52ea0d66993cfb22904deb
Alice Thammavong <thammavongalice@gmail.com>
2019-11-09 23:31:59 +0100

    Delete compo Trailer. Add placeholder attribute in LogInform

--- frontend/src/components/LogInForm.jsx
+++ frontend/src/components/LogInForm.jsx
@@ -6,43 +6,51 @@ class LogInForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "Username",
-      password: "Password"
+      username: "",
+      password: ""
     };
   }
+  handleSubmit = e => {
+    e.preventDefault();
+  };
 
   render() {
     return (
       <div className="logIn">
         <h1 className="form">Log In</h1> 
         <label className="label">Username</label>
-                  
-        <input
-          className="logInInput"
-          type="text"
-          value={this.state.username}
-          onChange={event => {
-            this.setState({ username: event.target.value });
-          }}
-        />
-        <label className="label">Password</label>
-                  
-        <input
-          className="logInInput"
-          type="text"
-          value={this.state.password}
-          onChange={event => {
-            this.setState({ password: event.target.value });
-          }}
-        />
-        <p className="signUp">
-          <Link to="/signInForm">Don't have an account yet? Sign Up!</Link>>
-        </p>{" "}
-                  
-        <button type="submit" className="submitButton">
-          Submit!
-        </button>
-                
+                   
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            className="logInInput"
+            type="text"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={event => {
+              this.setState({ username: event.target.value });
+            }}
+          />
+          <label className="label">Password</label>
+                    
+          <input
+            className="logInInput"
+            type="text"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={event => {
+              this.setState({ password: event.target.value });
+            }}
+          />
+          <p className="signUp">
+            <Link to="/signInForm">Don't have an account yet? Sign Up!</Link>>
+          </p>{" "}
+                    
+          <button type="submit" className="submitButton">
+            Submit!
+          </button>
+             
+        </form>
+             
       </div>
     );
   }
