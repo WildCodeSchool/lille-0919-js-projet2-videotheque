35672afec5964689c3b93adc8087c57a3cde3af2
Alice Thammavong <thammavongalice@gmail.com>
2019-11-12 09:33:09 +0100

    Create class for SignUpand LogIn

--- frontend/src/components/ModalContent.jsx
+++ frontend/src/components/ModalContent.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import LogInForm from "./LogInForm";
+import SignUpForm from "./SignUpForm";
 import "./style/LogInForm.css";
 
-function ModalContent({ toggleModal, handleLogIn }) {
-  return (
-    <aside className="modal-content">
-      <div>
-        <LogInForm toggleModal={toggleModal} handleLogIn={handleLogIn} />
+class ModalContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { signUpActive: false };
+  }
 
-        <button className="buttonClose" onClick={() => toggleModal()}>
-          Close
-        </button>
-      </div>
-    </aside>
-  );
+  toggleSignUp = () => {
+    this.setState({ signUpActive: !this.state.signUpActive });
+  };
+
+  render() {
+    const { toggleModal, handleLogIn } = this.props;
+    return (
+      <aside className="modal-content">
+        <div>
+          {this.state.signUpActive ? (
+            <SignUpForm
+              toggleSignUp={this.toggleSignUp}
+              toggleModal={toggleModal}
+              handleLogIn={handleLogIn}
+            />
+          ) : (
+            <LogInForm
+              toggleSignUp={this.toggleSignUp}
+              toggleModal={toggleModal}
+              handleLogIn={handleLogIn}
+            />
+          )}
+
+          <button className="buttonClose" onClick={() => toggleModal()}>
+            Close
+          </button>
+        </div>
+      </aside>
+    );
+  }
 }
 
 export default ModalContent;
